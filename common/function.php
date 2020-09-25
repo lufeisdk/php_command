67abbc61299a719fd3f88e703842669d0c22b31d
lufei <qiuxinke2006@126.com>
2020-09-25 17:36:28 +0800

    增加自定义函数方法array_get

--- common/function.php
+++ common/function.php
@@ -4,3 +4,30 @@ function pp($var)
     echo '<pre>';
     print_r($var);
 }
+
+/**
+ * array_get方法使用"."号从嵌套数组中获取值
+ * @param array $array
+ * @param $key
+ * @param $default
+ * @return mixed|null
+ */
+function array_get($array, $key, $default = null)
+{
+    if (is_null($key)) {
+        return $array;
+    }
+
+    if (isset($array[$key])) {
+        return $array[$key];
+    }
+
+    foreach (explode('.', $key) as $segment) {
+        if (!is_array($array) || !array_key_exists($segment, $array)) {
+            return $default;
+        }
+
+        $array = $array[$segment];
+    }
+    return $array;
+}
